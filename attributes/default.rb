1464d873bb581e768613c0d4fa8011b60bc4820d
moznion <moznion@gmail.com>
2013-11-13 18:23:58 +0900

    Add function for basic installation

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+CUDA_VERSION = '5.5'
+
+default['cuda']['packages'] = ['cuda']
+default['cuda']['version']  = CUDA_VERSION
+
+case platform_family
+when "rhel"
+  default['cuda']['pkg_manager_src'] = 'http://developer.download.nvidia.com/compute/cuda/repos/rhel6/x86_64/cuda-repo-rhel6-' << CUDA_VERSION << '-0.x86_64.rpm'
+  default['cuda']['pkg_manager']     = "#{Chef::Config[:file_cache_path]}/cuda.rpm"
+  default['cuda']['profile']         = "#{ENV['HOME']}/.bashrc"
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,61 @@
-#
-# Cookbook Name:: cuda-cookbook
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+case node['platform_family']
+when 'rhel'
+  p node['cuda']['pkg_manager_src']
+  remote_file node['cuda']['pkg_manager'] do
+    action :create_if_missing
+    source node['cuda']['pkg_manager_src']
+  end
+
+  rpm_package node['cuda']['pkg_manager'] do
+    action :install
+    source node['cuda']['pkg_manager']
+  end
+
+  packages = %w/gcc-c++ kernel-devel/
+  packages.each do |pkg|
+    yum_package pkg do
+      action :install
+      flush_cache [:before]
+    end
+  end
+
+  packages = %w/nvidia-settings nvidia-kmod xorg-x11-drv-nouveau/
+  packages.each do |pkg|
+    yum_package pkg do
+      action :remove
+      flush_cache [:before]
+    end
+  end
+
+  packages.each do |pkg|
+    yum_package pkg do
+      action :install
+      flush_cache [:before]
+    end
+  end
+end
+
+node['cuda']['packages'].each do |pkg|
+  package pkg do
+    action  :install
+    options options
+  end
+end
+
+cuda_home = 'export CUDA_HOME=/usr/local/cuda-5.5'
+execute 'Set CUDA_HOME' do
+  command %/echo "#{cuda_home}" >> #{node['cuda']['profile']}/
+  not_if { File.open(node['cuda']['profile']).read.match(cuda_home) }
+end
+
+ld_library_path = 'export LD_LIBRARY_PATH=\$CUDA_HOME/lib64:\$LD_LIBRARY_PATH'
+execute 'Set LD_LIBRARY_PATH' do
+  command %/echo "#{ld_library_path}" >> #{node['cuda']['profile']}/
+  not_if { File.open(node['cuda']['profile']).read.match(ld_library_path) }
+end
+
+path = 'export PATH=\$CUDA_HOME/bin:\$PATH'
+execute 'Set PATH' do
+  command %/echo "#{path}" >> #{node['cuda']['profile']}/
+  not_if { File.open(node['cuda']['profile']).read.match(path) }
+end
