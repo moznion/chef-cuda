4a16ab5e108a92e9d866755b68a843336b266954
moznion <moznion@gmail.com>
2013-11-14 20:04:37 +0900

    Change target profile file

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,5 +7,5 @@ case platform_family
 when "rhel"
   default['cuda']['pkg_manager_src'] = 'http://developer.download.nvidia.com/compute/cuda/repos/rhel6/x86_64/cuda-repo-rhel6-' << CUDA_VERSION << '-0.x86_64.rpm'
   default['cuda']['pkg_manager']     = "#{Chef::Config[:file_cache_path]}/cuda.rpm"
-  default['cuda']['profile']         = "#{ENV['HOME']}/.bashrc"
+  default['cuda']['profile']         = '/etc/bashrc'
 end
